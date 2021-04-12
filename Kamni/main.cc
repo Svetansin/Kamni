95b8810ea2694b5cbfebaef69f8adeba9ce78798
Svetansin <71337785+Svetansin@users.noreply.github.com>
2021-04-12 19:35:39 +0500

    Update main.cc

--- Kamni/main.cc
+++ Kamni/main.cc
@@ -1,7 +1,3 @@
-git config --global i18n.commitencodig windows-1252
-git config --global i18n.logoutputencoding windows-1252
-set LESSCHARSET=latin1
-
 #include <random>
 #include <iostream>
 #include <chrono>
